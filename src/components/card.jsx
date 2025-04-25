bb2a87032bf2e1b0a5e5049f1eff90779c47cc36
Daniel <dabies11@gmail.com>
2025-04-24 22:04:59 -0700

    Styled cards with image, made them controllable
    Added styling to the cards so that they will be able to handle the
    images that we will be giving them, and added props to be passed in

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,3 +1,11 @@
-export default function Card( {children} ) {
-    return <div className="image-card">{children}</div>
+import './card.css'
+
+export default function Card({ image = '../public/vite.svg', name = 'Pokemon Name', handleClick }) {
+    return (
+        <div className="image-card" onClick={handleClick}>
+            <img src={image}></img>
+            <h3>{name}</h3>
+        </div>
+    )
+    
 }
